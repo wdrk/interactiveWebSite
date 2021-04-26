3d2161964004dd2b85adb946c43130f3f8cee290
WDRK <lllrrrlrssr@gmail.com>
2021-04-27 01:15:36 +0900

    createElement, innerHTML, appendChild, removeChild 명령어로 엘리먼트 생성, 등록, 추가, 삭제

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const items = document.querySelector('.characters');
-items.setAttribute('data-id', 123);
-const attr = items.getAttribute('data-id');
-console.log(`getAttribute : ${attr}`);
+// 엘리먼트 생성
+const elem = document.createElement('p');
+
+// 엘리먼트에 내용을 설정
+elem.innerHTML = '<a href="#">Hello</a>???';
+
+// 원하는 엘리먼트를 가져와서 자식 엘리먼트로 등록하기
+const charactersElem = document.querySelector('.characters');
+charactersElem.appendChild(elem);
+
+// 자식 엘리먼트 삭제
+charactersElem.removeChild(document.querySelector('.b'));
