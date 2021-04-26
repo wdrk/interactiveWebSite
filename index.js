2b20b4cebef4bcab3ccce2bbc89a93bb0c83fd01
WDRK <lllrrrlrssr@gmail.com>
2021-04-27 01:04:07 +0900

    characters클래스에 표준 커스텀 속성을 추가하는 예제

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-// 최초로 선언한 클래스만 선택하는 방법
-console.log(document.querySelector('.ilbuni'));
-
-// 특정 클래스를 가진 전부를 선택하는 방법
-console.log(document.querySelectorAll('.ilbuni'));
-
-// 유사배열로 가져와서 일부를 선택하는 방법
-const ilbuniGroup = document.querySelectorAll('.ilbuni');
-console.log(ilbuniGroup[2]);
+const items = document.querySelector('.characters');
+items.setAttribute('data-id', 123);
