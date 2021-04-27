73f91d1f9128e4c0a979b78261ae6f24294c475b
WDRK <lllrrrlrssr@gmail.com>
2021-04-27 11:21:57 +0900

    addEventListener()로 click 이벤트를 감지해서 태그에 속성을 추가하는 예제

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const item = document.querySelector('.ilbuni');
-
-// 기존의 클래스를 그대로 두고 새로운 클래스를 추가
-item.classList.add('special');
-
-// 기존의 클래스를 모두 지우고 새로운 클래스로 설정
-item.className = 'special';
-
-// 클래스 삭제
-const item2 = document.querySelector('.ilbuni');
-item2.classList.remove('ilbuni');
-
-// 클래스 껐다켰다 가능
-item2.classList.toggle('special');
+const ilbuniC = document.querySelector('.ilbuni.c');
+ilbuniC.addEventListener('click', () => {
+  ilbuniC.classList.add('special');
+});
