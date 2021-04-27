201f36d34a45711daab2543922189bea7168a302
WDRK <lllrrrlrssr@gmail.com>
2021-04-27 16:56:55 +0900

    html에 reset.css를 적용하고 index.js와 style.css를 초기화

--- index.js
+++
@@ -1,9 +0,0 @@
-const stage = document.querySelector('.stage');
-function clickHandler(e) {
-  if (e.target.classList.contains('ilbuni')) {
-    stage.removeChild(e.target);
-  } else {
-    console.log('clicked stage');
-  }
-}
-stage.addEventListener('click', clickHandler);
