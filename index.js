4832fb23fc779b1a26aa9ddd9b4642a867835583
WDRK <lllrrrlrssr@gmail.com>
2021-04-28 15:35:28 +0900

    문을 열 때 이미 열려있는 문은 닫도록 index.js를 구성함

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const stageElem = document.querySelector('.stage');
+let currentElem;
 function doorHandler(e) {
+  if (currentElem) {
+    currentElem.classList.remove('door-opened');
+  }
   if (e.target.classList.contains('door-body')) {
     e.target.parentNode.classList.toggle('door-opened');
+    currentElem = e.target.parentNode;
   }
 }
 stageElem.addEventListener('click', doorHandler);
