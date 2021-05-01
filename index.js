a853ba2c3040c6315ac054add4692695bdea6941
WDRK <lllrrrlrssr@gmail.com>
2021-05-01 17:54:18 +0900

    웹페이지 스크롤을 감지해서 값을 가져오는 pageYOffset 설정

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const stageElem = document.querySelector('.stage');
-let currentElem;
-function closeTheDoor(elem) {
-  elem.classList.remove('door-opened');
-}
-function openTheDoor(elem) {
-  elem.classList.toggle('door-opened');
-  currentElem = elem;
-}
-function doorHandler(e) {
-  if (currentElem) {
-    closeTheDoor(currentElem);
-  }
-  if (e.target.classList.contains('door-body')) {
-    openTheDoor(e.target.parentNode);
-  }
-}
-stageElem.addEventListener('click', doorHandler);
+const outputElem = document.querySelector('.output');
+window.addEventListener('scroll', function () {
+  outputElem.innerHTML = window.pageYOffset;
+});
