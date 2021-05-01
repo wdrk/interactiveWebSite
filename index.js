9aab55c137ee267f21e64116bb1b1eb86ed5e56f
WDRK <lllrrrlrssr@gmail.com>
2021-05-01 18:54:29 +0900

    브라우저 top을 기준으로 객체의 위치값을 가져오는 방법

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const outputElem = document.querySelector('.output');
+const ilbuniElem = document.querySelector('.ilbuni');
+
 window.addEventListener('scroll', function () {
-  outputElem.innerHTML = window.pageYOffset;
+  // outputElem.innerHTML = window.pageYOffset; 스크롤을 감지해서 값을 가져오는 방법
+  // outputElem.innerHTML = ilbuniElem.offsetTop; 객체의 처음 위치값을 가져오는 방법
+  //
+  // 각종 위치값을 가져오는 방법
+  // outputElem.innerHTML = ilbuniElem.getBoundingClientRect();
+  // 브라우저 top을 기준으로 객체의 위치값 가져오기
+  outputElem.innerHTML = ilbuniElem.getBoundingClientRect().top;
 });
