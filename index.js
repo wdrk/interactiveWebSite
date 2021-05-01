1f6ca5f6f66637abcfbaa57a80a391ee1c0c1b83
WDRK <lllrrrlrssr@gmail.com>
2021-05-01 19:02:49 +0900

    객체가 브라우저의 일정 위치 이상이면 크기를 2배로 변경하는 예제

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const outputElem = document.querySelector('.output');
 const ilbuniElem = document.querySelector('.ilbuni');
 
-window.addEventListener('scroll', function () {
-  // outputElem.innerHTML = window.pageYOffset; 스크롤을 감지해서 값을 가져오는 방법
-  // outputElem.innerHTML = ilbuniElem.offsetTop; 객체의 처음 위치값을 가져오는 방법
-  //
-  // 각종 위치값을 가져오는 방법
-  // outputElem.innerHTML = ilbuniElem.getBoundingClientRect();
-  // 브라우저 top을 기준으로 객체의 위치값 가져오기
-  outputElem.innerHTML = ilbuniElem.getBoundingClientRect().top;
-});
+function showValue() {
+  let posY = ilbuniElem.getBoundingClientRect().top;
+  outputElem.innerHTML = posY;
+
+  if (posY < window.innerHeight * 0.2) {
+    ilbuniElem.classList.add('zoom');
+  } else {
+    ilbuniElem.classList.remove('zoom');
+  }
+}
+
+window.addEventListener('scroll', showValue);
