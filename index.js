e02e7834ae54eca8e93a776c74a24e98371f5369
WDRK <lllrrrlrssr@gmail.com>
2021-05-02 11:16:33 +0900

    @keyframes와 animation 속성으로 공을 움직이게 함

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 const ballElem = document.querySelector('.ball');
-
-function clickHandler(event) {
-  ballElem.style.transform = `translate(${event.clientX - 15}px, ${
-    event.clientY - 15
-  }px)`;
-}
-
-function endHandler(event) {
-  ballElem.classList.add('end');
-}
-
-window.addEventListener('click', clickHandler);
-ballElem.addEventListener('transitionend', endHandler);
