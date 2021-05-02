679fd7f15687e0f3182997642fa7bebb839ec463
WDRK <lllrrrlrssr@gmail.com>
2021-05-02 11:24:32 +0900

    click이벤트 함수에서 엘리먼트의 style에 접근해서 직접 css 속성을 추가하는 방법

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const ballElem = document.querySelector('.ball');
+
+ballElem.addEventListener('click', function () {
+  ballElem.style.animation = 'ball-ani 1s 3 alternate forwards';
+});
+
 ballElem.addEventListener('animationend', function () {
   ballElem.classList.add('end');
 });
