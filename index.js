124c0a1f8e29cc0a99383dcfcc5ff8bc2861a4fc
WDRK <lllrrrlrssr@gmail.com>
2021-05-02 11:59:23 +0900

    설정한 시간마다 반복 수행하는 setInterval() 함수 기본 호출

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 'use strict';
 
-const timeId = setTimeout(() => {
-  console.log('setTimeout!');
-}, 3000);
-
-console.log(timeId);
-
-const btnElem = document.querySelector('.btn');
-btnElem.addEventListener('click', function () {
-  clearTimeout(timeId);
-});
+const sample = () => {
+  console.log('Interval');
+};
+let timeId = setInterval(sample, 1000);
