54604e0077e3433da031aa18786f34638b31ea14
WDRK <lllrrrlrssr@gmail.com>
2021-05-02 11:28:51 +0900

    animation을 반복하는 이벤트를 감지하는 animationiteration 적용

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const ballElem = document.querySelector('.ball');
 
 ballElem.addEventListener('click', function () {
-  ballElem.style.animation = 'ball-ani 1s 3 alternate forwards';
+  ballElem.style.animation = 'ball-ani 1s 3 forwards';
 });
 
 ballElem.addEventListener('animationend', function () {
   ballElem.classList.add('end');
+});
+
+ballElem.addEventListener('animationiteration', function () {
+  console.log('반복!');
 });
